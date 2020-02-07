dcb1574ba1c849c6dbd174c5bc4b5679d9190b0f
Alban <albanbleicher>
2020-02-08 00:59:57 +0100

    rename dossier projets

--- src/images.js
+++ src/images.js
@@ -1,21 +1,21 @@
 export const apdc = [
-  require('./assets/images/mediasProjects/apdc/1.png'),
-  require('./assets/images/mediasProjects/apdc/2.png'),
-  require('./assets/images/mediasProjects/apdc/3.png'),
+  require('./assets/images/projects/apdc/1.png'),
+  require('./assets/images/projects/apdc/2.png'),
+  require('./assets/images/projects/apdc/3.png'),
 ]
 export const webdoc = [
-  require('./assets/images/mediasProjects/webdoc/1.png'),
-  require('./assets/images/mediasProjects/webdoc/2.png'),
-  require('./assets/images/mediasProjects/webdoc/3.png'),
-  require('./assets/images/mediasProjects/webdoc/4.png')
+  require('./assets/images/projects/webdoc/1.png'),
+  require('./assets/images/projects/webdoc/2.png'),
+  require('./assets/images/projects/webdoc/3.png'),
+  require('./assets/images/projects/webdoc/4.png')
 ]
 export const health_a_week = [
-  require('./assets/images/mediasProjects/health_a_week/1.jpg'),
-  require('./assets/images/mediasProjects/health_a_week/2.jpg'),
-  require('./assets/images/mediasProjects/health_a_week/3.jpg'),
-  require('./assets/images/mediasProjects/health_a_week/4.jpg')
+  require('./assets/images/projects/health_a_week/1.jpg'),
+  require('./assets/images/projects/health_a_week/2.jpg'),
+  require('./assets/images/projects/health_a_week/3.jpg'),
+  require('./assets/images/projects/health_a_week/4.jpg')
 ]
-export const digitalHapiness = require('./assets/images/mediasProjects/digital-hapiness.png');
+export const digitalHapiness = require('./assets/images/projects/digital-hapiness.png');
 export const right_arrow = require('./assets/images/right_arrow.svg');
 export const up_arrow = require('./assets/images/up_arrow.svg');
 export const left_arrow = require('./assets/images/left_arrow.svg');
