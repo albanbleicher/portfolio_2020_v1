b2d31e103db9ef96d02cc47bd359dc478659d4e0
Alban <albanbleicher>
2020-02-12 12:00:38 +0100

    test Tweenmax curseur

--- public/actionScroll.js
+++ public/actionScroll.js
@@ -1,9 +1,38 @@
-// "use strict";
-// window.addEventListener("DOMContentLoaded", (e) => {
-// document.body.addEventListener('mousemove', function(e) {
-//   // document.getElementById('mouse').style.left = e.clientX - 10 + 'px';
-//   // document.getElementById('mouse').style.top = e.clientY - 10 + 'px';
-//
-//   gsap.to("#mouse", {duration: 2, x: 300});
-// })
-// });
+$(document).ready(function(e) {
+  var cursor = $("#mouse");
+
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.0015, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+
+    TweenMax.set(cursor, {
+        css: {
+        left: posX-7.5,
+        top: posY-7.5
+        }
+    });
+  }
+});
+
+$(document).on("mousemove", function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+// yellow circle
+$(".rightArrow, .leftArrow, a").on("mouseenter", function() {
+    cursor.addClass("hoverCursor");
+    // follower.addClass("active");
+});
+$(".rightArrow, .leftArrow, a").on("mouseleave", function() {
+    cursor.removeClass("hoverCursor");
+    // follower.removeClass("active");
+});
+})
